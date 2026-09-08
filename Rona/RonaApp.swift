eaa73b6f97913afa9f469df4d49173e4d97b174f
raddhuha <radianfarsainsa@gmail.com>
2026-09-08 10:41:02 +0700

    Initial Commit

---
+++ Rona/RonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RonaApp.swift
+//  Rona
+//
+//  Created by Radian Dhuha on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
